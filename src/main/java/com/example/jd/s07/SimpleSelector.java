d8323f4f5f139c01b738698f5a45c0d3fd890c61
Emanuele <egalli64@gmail.com>
2024-11-13 08:39:17 +0100

    clarify

--- src/main/java/com/example/jd/s07/SimpleSelector.java
+++ src/main/java/com/example/jd/s07/SimpleSelector.java
@@ -24,7 +24,7 @@ public class SimpleSelector {
     private static final Logger log = LoggerFactory.getLogger(SimpleSelector.class);
 
     private static final String GET_CODERS = """
-            SELECT employee_id, first_name, last_name
+            SELECT e.employee_id, e.first_name, e.last_name
             FROM employee e JOIN department d
             USING (department_id)
             WHERE d.name = 'IT'""";
@@ -41,14 +41,14 @@ public class SimpleSelector {
                 Statement stmt = conn.createStatement();
                 ResultSet rs = stmt.executeQuery(GET_CODERS)) {
             log.debug("Looping on the result set");
-            System.out.printf("%4s %20s %20s%n", "id", "first", "last");
+            System.out.printf("%4s %20s %20s\n", "id", "first", "last");
 
             while (rs.next()) {
                 int id = rs.getInt(1);
                 String first = rs.getString(2);
                 String last = rs.getString(3);
 
-                System.out.printf("%4d %20s %20s%n", id, first, last);
+                System.out.printf("%4d %20s %20s\n", id, first, last);
             }
             log.debug("Done");
         } catch (SQLException se) {
